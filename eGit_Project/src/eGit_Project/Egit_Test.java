0164fead1edc5d9962ce392239c4d97ca46c7da5
GopinathCTS <gopinath.nagarajan@gmail.com>
2016-11-24 21:56:48 +0530

    Modification made by Gopi

--- eGit_Project/src/eGit_Project/Egit_Test.java
+++ eGit_Project/src/eGit_Project/Egit_Test.java
@@ -4,7 +4,7 @@ public class Egit_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Current class name : ");
 	}
 
 }
--- eGit_Project/src/eGit_Project/Egit_Test2.java
+++ eGit_Project/src/eGit_Project/Egit_Test2.java
@@ -1,5 +1,6 @@
 package eGit_Project;
 
 public class Egit_Test2 {
-
+//Please switch to a page other than ‘Detailed Profile’ in My Profile App for Copy (CTRL+C) / Cut (CTRL+X) operations to work
+	
 }
